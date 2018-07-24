c1442c41154c3c0755ebf0c00e53f5586f945e1f
Guanrao <guanrao@yahoo.com>
2018-07-24 09:35:40 -0400

    story 1

--- Main.java
+++ Main.java
@@ -15,6 +15,14 @@ public class Main {
     }
 
     public static void main(String... args) {
-        
+        StringBuilder sb = new StringBuilder();
+        try{
+            sb.append("connect smtp\nTo: ").append(args[0]).append("\n\n").append(args[1]).append("\n\n").append("disconnect\n");
+
+            network.write(sb.toString());
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+
     }
 }
